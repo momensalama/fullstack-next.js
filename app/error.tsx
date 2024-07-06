6f11730589dcb2429c374d82c8e08d22956d4fd9
momen salama <momensalama712@gmail.com>
2024-07-06 18:54:00 +0300

    add reset function to error component

--- app/error.tsx
+++ app/error.tsx
@@ -1,17 +1,27 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+
 interface ErrorProps {
   error: Error;
   reset: () => void;
 }
 
 export default function Error({ error, reset }: ErrorProps) {
+  const router = useRouter();
+  // function handleReset() {
+  //   router.reload();
+  // }
+
   return (
     <main className="flex justify-center items-center flex-col gap-6">
       <h1 className="text-3xl font-semibold">Something went wrong!</h1>
       <p className="text-lg">{error.message}</p>
 
-      <button className="inline-block bg-accent-500 text-primary-800 px-6 py-3 text-lg">
+      <button
+        className="inline-block bg-accent-500 text-primary-800 px-6 py-3 text-lg"
+        onClick={() => reset()}
+      >
         Try again
       </button>
     </main>
